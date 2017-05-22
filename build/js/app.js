1ce9e964aedf829e0b02f3b840f2f6546ab8a56a
Ken Rutan <kenr16@gmail.com>
2017-05-22 15:37:26 -0700

    add API call and API key as a hidden file.

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "4df2f506980b4408686c19d278f83ad1";
+
+},{}],2:[function(require,module,exports){
 function Journal(name) {
   this.name = name;
 }
@@ -23,7 +26,7 @@ Journal.prototype.getTeaser = function(entry) {
 
 exports.journalModule = Journal;
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 var Journal = require('./../js/journal.js').journalModule;
 
 $(document).ready(function() {
@@ -44,4 +47,49 @@ $(document).ready(function() {
   });
 });
 
-},{"./../js/journal.js":1}]},{},[2]);
+$(document).ready(function(){
+  $('#time').text(moment());
+});
+
+var apiKey = require('./../.env').apiKey;
+
+// $(document).ready(function() {
+//   $('#weather-location').click(function() {
+//     var city = $('#location').val();
+//     $('#location').val("");
+//
+//     $('.showWeather').text("The city you have chosen is " + city + ".");
+//     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+//       $('.showWeather').append("The humidity in " + city + " is " + response.main.humidity + "%");
+//       $('.showWeather').append("The weather in " + city + " is " + response.weather[0].description + "!");
+//       console.log(JSON.stringify(response));
+//       console.log(response);
+//     });
+//
+//     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+//       .then(function(response) {
+//         $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+//       });
+//
+//       console.log("Notice: The GET request has been made.");
+//
+//   });
+// });
+
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+      .then(function(response){
+        $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+     })
+
+     .fail(function(error) {
+        $('.showWeather').text(error.responseJSON.message);
+      });
+  });
+});
+
+},{"./../.env":1,"./../js/journal.js":2}]},{},[3]);
--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,27 +1,3 @@
-var Journal = require('./../js/journal.js').journalModule;
-
-$(document).ready(function() {
-  $("#journal-entry").submit(function(event){
-    event.preventDefault();
-      let entry = {
-        title: $("#title").val(),
-        body: $("#body").val()
-      }
-      const diary = new Journal('New Journal');
-      let consonants = diary.consonants(entry);
-      let vowels = diary.vowels(entry);
-      let words = diary.words(entry);
-      let teaser = diary.getTeaser(entry);
-      $("#journal-info").append("<p>" + teaser + "... This entry contains " + words + " words, " + consonants + " consonants and " + vowels + " vowels.</p>");
-      $("#title").text("");
-      $("#body").text("");
-  });
-});
-
-$(document).ready(function(){
-  $('#time').text(moment());
-});
-
 var apiKey = require('./../.env').apiKey;
 
 // $(document).ready(function() {
